40d833237ce24c98683a7f75d6dfacc6401fa661
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2021-08-19 21:41:12 +0800

    init

---
+++ components/Sidebar.php
@@ -0,0 +1,95 @@
+<div class="sidebar">
+    <div class="sidebar-title">
+      <a href="../" class="brand-link text-center"> 
+        <img src="../dist/img/favicon.gif" alt="Logo" class="brand-image elevation-3" >
+        <span class="brand-text font-weight-bold text-light" >TaskGuard</span>
+      </a>
+    </div>
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Task Calendar
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/client" class="nav-link">
+            <i class="nav-icon fas fa-chart-bar"></i>
+              <p>
+                Reports
+              </p>
+              <i class="right fas fa-angle-left"></i>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item has-treeview">
+                <a href="#" target="_blank" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Task Search</p>
+                </a>
+              </li>
+              <li class="nav-item has-treeview">
+                <a href="#" target="_blank" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>User Activity Log</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa fa-play nav-icon" aria-hidden="true"></i>
+              <p>
+                Next Task
+              </p>
+            </a>
+            
+          </li>
+          <li class="nav-item">
+            <a href="pages/emailtemplates" class="nav-link">
+            <i class="nav-icon fa fa-tasks" aria-hidden="true"></i>
+              <p>
+                Tasks
+              </p>
+              <i class="right fas fa-angle-left"></i>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item has-treeview">
+                <a href="#" target="_blank" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Task Search</p>
+                </a>
+              </li>
+              <li class="nav-item has-treeview">
+                <a href="#" target="_blank" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>User Activity Log</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="php/logout" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
