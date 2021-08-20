829998124f5518b85dbeb0140e73c0dee72da152
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2021-08-20 21:22:55 +0800

    checkbox

--- nexttask.php
+++ nexttask.php
@@ -172,8 +172,6 @@ sagittis lacus vel augue laoreet rutrum faucibus.">
     toastr.options.closeButton = true;
     $.widget.bridge('uibutton', $.ui.button);
     $('[data-toggle="popover"]').popover();
-
-
       var dt = $('#dataTable').DataTable({
         "oLanguage": {
           "sLengthMenu": "Show Entries _MENU_",
@@ -202,14 +200,16 @@ sagittis lacus vel augue laoreet rutrum faucibus.">
           var btnStart= '<button type="button" class="btn btn-primary" onclick="refreshTable()">Start</button>';
           var btnPause='<button type="button" class="btn btn-success">Pause</button>';
           var btnStop='<button type="button" class="btn btn-danger">Stop</button>';
+          var cb ='';
           data = JSON.parse(response);
           dt.clear().draw();
           for(var da in data){
             btnStart= '<button type="button" class="btn btn-primary" value="'+data[da].callback_id+'">Start</button>';
             btnPause='<button type="button" class="btn btn-success" value="'+data[da].callback_id+'">Pause</button>';
             btnStop='<button type="button" class="btn btn-danger" value="'+data[da].callback_id+'">Stop</button>';
+            cb ='<input class="form-control" type="checkbox" name="list[]" value="'+data[da].callback_id+'">';
             dt.row.add([
-              data[da].callback_id,
+              cb,
               data[da].TaskName,
               data[da].client_name,
               data[da].Notes,
