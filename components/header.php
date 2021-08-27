3a7f9cae1f1aad9919340d2646ae512f9cce81c3
NORRIS HIPOLITO <NORRISHIPOLITO@example.com>
2021-08-27 19:11:13 +0800

    fix table and css

--- components/header.php
+++ components/header.php
@@ -17,10 +17,13 @@ $userinfo = $_SESSION['userInfo'];
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="./plugins/daterangepicker/daterangepicker.css">
     <!-- DataTables -->
     <link rel="stylesheet" href="./plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="./plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
     <link rel="stylesheet" href="./plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="./plugins/datatables-select/css/select.bootstrap4.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- CUSTOM CSS -->
